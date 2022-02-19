c6d87023c77a7fe2f2bd47b23c7c2c36674c38f1
omkar <omkar@LAPTOP-Q6BPOKUJ>
2022-02-19 17:47:52 +0530

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,139 @@
+﻿using DotNetExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetExam.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True";
+            con.Open();
+            SqlCommand cmdDisplay = new SqlCommand();
+            cmdDisplay.Connection = con;
+            cmdDisplay.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdDisplay.CommandText = "FetchProducts";
+            List<Products> prd = new List<Products>();
+            try
+            {
+                SqlDataReader dre = cmdDisplay.ExecuteReader();
+                while (dre.Read())
+                {
+                    prd.Add(new Products
+                    {
+                        ProductId = (int)dre["ProductId"],
+                        ProductName = dre["ProductName"].ToString(),
+                        Description = dre["Description"].ToString(),
+                        Rate = (int)(decimal)dre["Rate"],
+                        CategoryName = dre["CategoryName"].ToString()
+
+                    });
+                 }
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                con.Close();
+            }
+            return View(prd);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Products prd = new Products();
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Products prd)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DotNetExam;Integrated Security=True";
+            con.Open();
+            SqlCommand cmdEdit = new SqlCommand();
+            cmdEdit.Connection = con;
+            cmdEdit.CommandType = System.Data.CommandType.StoredProcedure;
+            cmdEdit.CommandText = "UpdateProducts";
+            cmdEdit.Parameters.AddWithValue("@ProductId", id);
+            cmdEdit.Parameters.AddWithValue("@ProductName", prd.ProductName);
+            cmdEdit.Parameters.AddWithValue("@Description", prd.Description);
+            cmdEdit.Parameters.AddWithValue("@Rate", prd.Rate);
+            cmdEdit.Parameters.AddWithValue("@CategoryName", prd.CategoryName);
+            try
+            {
+                cmdEdit.ExecuteNonQuery();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DotNetExam.Models
+{
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Product Name")]
+        public string ProductName { get; set; }
+        [DataType(DataType.Currency)]
+        [Required(ErrorMessage = "Please Enter Rate")]
+        public int Rate { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Write Some Description about product")]
+        [StringLength(250, ErrorMessage = "The {0} many Characters of Description you entered it exceeds the limit{1} characters" )]
+        public string Description { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Please Enter Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
